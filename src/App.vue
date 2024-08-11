d6553f9a2378f872c39ef5d4b6fcb7194e8806a5
Jiahang Sun <jsun0096@student.monash.edu>
2024-08-11 23:26:59 +1000

    eFolio 3&4 Activity 1-7

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-import JSON from './components/JSON.vue'
+// import JSON from './components/JSON.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
   <header>
-    <JSON />
+    <!-- <JSON /> -->
+    <Form />
   </header>
 </template>
 
