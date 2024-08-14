ff2d892e15f7e1a16729b8b21a978ed595e4fd98
Jiahang Sun <jsun0096@student.monash.edu>
2024-08-14 14:16:45 +1000

    import primevue

--- src/main.js
+++ src/main.js
@@ -7,4 +7,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+const app  = createApp(App);
+app.use(PrimeVue, {
+    theme:{
+        preset: Aura
+    }
+});
+
 createApp(App).mount('#app')
